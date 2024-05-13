db1823bdb5ab493ac58c896082f7e49e6cdbb287
Yajuvendra <yajuvendra.singh@gmail.com>
2024-05-13 12:16:22 +0800

    fixed the syntax

--- redhatai.py
+++ redhatai.py
@@ -62,7 +62,7 @@ def load_model():
         load_in_4bit=True,
         bnb_4bit_quant_type='nf4',
         bnb_4bit_use_double_quant=True,
-        bnb_4bit_use_double_quant = False,
+        bnb_4bit_use_double_quant=False,
         bnb_4bit_compute_dtype=torch.bfloat16)
     model = transformers.AutoModelForCausalLM.from_pretrained(model_dir, quantization_config=bnb_config,
                                                               torch_dtype=torch.bfloat16, device_map="auto", )
